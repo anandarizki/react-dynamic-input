8091951f4531c4495fb1281d8b0f76efafa634a8
Ananda Rizki <rizki.id@gmail.com>
2019-12-25 18:05:49 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hallo brader
+    </div>
+  );
+}
+
+export default App;
